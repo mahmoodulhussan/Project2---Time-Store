cb1fae26c3565eea875a48e7a0ea017449f05c0e
mahmoodulhussan <86261716+mahmoodulhussan@users.noreply.github.com>
2021-08-18 13:52:27 -0500

    mc resolved

--- Front-End/time-store-angular/src/app/User.ts
+++ Front-End/time-store-angular/src/app/User.ts
@@ -1,13 +1,9 @@
 export interface User{
     id?: number,
-<<<<<<< HEAD
-    email: String,
-    password:String 
-=======
     // username: string,
     email: string,
     password: string,
     firstName: string,
     lastName: string
->>>>>>> mahmoods-front-end-branch
+
 }
